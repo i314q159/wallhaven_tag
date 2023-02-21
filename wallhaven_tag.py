e30dc3d3dce4a529aa2a8fe4bef3927c09fe5a78
i314q159 <i314q159@outlook.com>
2023-02-21 08:44:33 +0800

    Update wallhaven_tag.py

--- wallhaven_tag.py
+++ wallhaven_tag.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 import requests
 
 wallhaven_key = "18nUEpLtkdrgmQpifc55Z1MAO8Cn7D44"
@@ -8,12 +11,14 @@ def wallhaven_json(wallhaven_api):
     return requests.get(wallhaven_api).json()
 
 
+def wallhaven_tag_page(wallhaven_key, tag_id):
+    return f"https://wallhaven.cc/api/v1/search?apikey={wallhaven_key}&q=id:{tag_id}&page="
+
+
 if __name__ == "__main__":
     img_paths = []
     tag_id = input("tag_id: ")
-    wallhaven_tag_page = (
-        f"https://wallhaven.cc/api/v1/search?apikey={wallhaven_key}&q=id:{tag_id}&page="
-    )
+    wallhaven_tag_page = wallhaven_tag_page(wallhaven_key, tag_id)
 
     tag_data = wallhaven_json(f"{wallhaven_tag_info}{tag_id}").get("data", None)
     match tag_data:
@@ -30,6 +35,7 @@ if __name__ == "__main__":
                 for img in img_dict[0:]:
                     img_paths.append(img.get("path", None))
                 print(f"page {page}")
+                time.sleep(random.random() * 3)
 
             with open(f"./{tag_name} {tag_id}.txt", "w") as f:
                 f.writelines(img_path + "\n" for img_path in img_paths)
