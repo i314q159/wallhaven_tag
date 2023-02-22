8ef69914e0f45fc495c818e51b14f9de0a43fd4c
i314q159 <i314q159@outlook.com>
2023-02-22 09:12:10 +0800

    Update wallhaven_tag.py

--- wallhaven_tag.py
+++ wallhaven_tag.py
@@ -1,10 +1,11 @@
+import json
 import random
 import time
 
 import requests
 
 wallhaven_key = "18nUEpLtkdrgmQpifc55Z1MAO8Cn7D44"
-wallhaven_tag_info = "https://wallhaven.cc/api/v1/tag/"
+wallhaven_tag = "https://wallhaven.cc/api/v1/tag/"
 
 
 def wallhaven_json(wallhaven_api):
@@ -20,12 +21,12 @@ if __name__ == "__main__":
     tag_id = input("tag_id: ")
     wallhaven_tag_page = wallhaven_tag_page(wallhaven_key, tag_id)
 
-    tag_data = wallhaven_json(f"{wallhaven_tag_info}{tag_id}").get("data", None)
-    match tag_data:
+    tag_info = wallhaven_json(f"{wallhaven_tag}{tag_id}").get("data", None)
+    match tag_info:
         case None:
             print("tag id is wrong")
         case _:
-            tag_name = tag_data.get("name")
+            tag_name = tag_info.get("name")
             last_page = (
                 wallhaven_json(f"{wallhaven_tag_page}1").get("meta").get("last_page")
             )
@@ -35,7 +36,6 @@ if __name__ == "__main__":
                 for img in img_dict[0:]:
                     img_paths.append(img.get("path", None))
                 print(f"page {page}")
-                time.sleep(random.random() * 3)
 
             with open(f"./{tag_name} {tag_id}.txt", "w") as f:
                 f.writelines(img_path + "\n" for img_path in img_paths)
