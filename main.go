4c251efd72f1589c414216122901189c313cc9ee
i314q159 <i314q159@outlook.com>
2023-03-13 21:49:26 +0800

    update

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const (
@@ -10,11 +12,33 @@ const (
 	WALLHAVEN_SEARCH = "https://wallhaven.cc/api/v1/search"
 )
 
-func main() {
-	id := "37"
-	fmt.Println(wallhaven_tag_page(id))
+func wallhaven_tag_info(id string) string {
+	return WALLHAVEN_TAG + id
 }
 
-func wallhaven_tag_page(s string) string {
-	return WALLHAVEN_SEARCH + "?apikey=" + WALLHAVEN_KEY + "&q=id:" + s + "&page="
+func wallhaven_tag_page(id string) string {
+	return WALLHAVEN_SEARCH + "?apikey=" + WALLHAVEN_KEY + "&q=id:" + id + "&page="
+}
+
+func wallhaven_json(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	// TODO: return json
+	return body
+}
+
+func main() {
+	id := "120"
+
+	fmt.Printf("%s\n", wallhaven_json(wallhaven_tag_info(id)))
+
+	fmt.Printf("%s\n", wallhaven_json(wallhaven_tag_page(id)+"1"))
 }
