46978f71cd341ca60580a20bd26b8981319a4249
i314q159 <i314q159@outlook.com>
2023-07-11 11:22:26 +0800

    thx chatgpt

--- wallhaven_tag.py
+++ wallhaven_tag.py
@@ -1,8 +1,9 @@
 import requests
+import os
 
-WALLHAVEN_KEY = "pRmbEacfPgeqeST5L2AH6qTEwxfN6SCO"
-WALLHAVEN_TAG = "https://wallhaven.cc/api/v1/tag/"
-WALLHAVEN_SEARCH = "https://wallhaven.cc/api/v1/search"
+wallhaven_key = "pRmbEacfPgeqeST5L2AH6qTEwxfN6SCO"
+wallhaven_tag = "https://wallhaven.cc/api/v1/tag/"
+wallhaven_search = "https://wallhaven.cc/api/v1/search"
 
 
 def wallhaven_json(wallhaven_api):
@@ -12,16 +13,16 @@ def wallhaven_json(wallhaven_api):
 
 
 def wallhaven_tag_page(wallhaven_key, id):
-    return f"{WALLHAVEN_SEARCH}?apikey={wallhaven_key}&q=id:{id}&page="
+    return f"{wallhaven_search}?apikey={wallhaven_key}&q=id:{id}&page="
 
 
 if __name__ == "__main__":
     img_paths = []
     tag_id = input("tag_id: ")
-    wallhaven_tag_page = wallhaven_tag_page(WALLHAVEN_KEY, tag_id)  # type: ignore
+    wallhaven_tag_page = wallhaven_tag_page(wallhaven_key, tag_id)  # type: ignore
 
     # "get" is not a known member of "None"
-    tag_info = wallhaven_json(f"{WALLHAVEN_TAG}{tag_id}").get("data", None)
+    tag_info = wallhaven_json(f"{wallhaven_tag}{tag_id}").get("data", None)
     match tag_info:
         case None:
             print("tag id is wrong")
@@ -33,11 +34,12 @@ if __name__ == "__main__":
 
             for page in range(1, last_page + 1):
                 img_dict = wallhaven_json(f"{wallhaven_tag_page}{page}").get("data")
-                for img in img_dict[0:]:
-                    img_paths.append(img.get("path", None))
+                page_img_paths = [img.get("path", None) for img in img_dict]
+                img_paths.extend(page_img_paths)
                 print(f"page {page}")
 
-            with open(f"./{tag_name}_{tag_id}.txt", "w") as f:
-                f.writelines(img_path + "\n" for img_path in img_paths)
+            file_path = os.path.join(".", f"{tag_name}_{tag_id}.txt")
+            with open(file_path, "w") as f:
+                f.writelines(f"{img_path}\n" for img_path in img_paths)
 
             print(f"{tag_name}_{tag_id}.txt, {len(img_paths)} line(s)")
