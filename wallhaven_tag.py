8730bee6561c5809c060ef2c65c77cac80c94c3c
i314q159 <i314q159@outlook.com>
2023-07-12 23:17:39 +0800

    Update wallhaven_tag.py

--- wallhaven_tag.py
+++ wallhaven_tag.py
@@ -1,15 +1,19 @@
-import requests
+#!/usr/bin/env python3
+
 import os
+import sys
+
+import requests
 
 wallhaven_key = "pRmbEacfPgeqeST5L2AH6qTEwxfN6SCO"
 wallhaven_tag = "https://wallhaven.cc/api/v1/tag/"
 wallhaven_search = "https://wallhaven.cc/api/v1/search"
 
 
-def wallhaven_json(wallhaven_api):
+def wallhaven_json(wallhaven_api, default=None):
     r = requests.get(wallhaven_api)
     if r.status_code == requests.codes.ok:
-        return r.json()
+        return r.json() or default
 
 
 def wallhaven_tag_page(wallhaven_key, id):
@@ -18,28 +22,29 @@ def wallhaven_tag_page(wallhaven_key, id):
 
 if __name__ == "__main__":
     img_paths = []
-    tag_id = input("tag_id: ")
+
+    tag_id = sys.argv[1]
     wallhaven_tag_page = wallhaven_tag_page(wallhaven_key, tag_id)  # type: ignore
 
-    # "get" is not a known member of "None"
-    tag_info = wallhaven_json(f"{wallhaven_tag}{tag_id}").get("data", None)
-    match tag_info:
-        case None:
-            print("tag id is wrong")
-        case _:
-            tag_name = tag_info.get("name")
-            last_page = (
-                wallhaven_json(f"{wallhaven_tag_page}1").get("meta").get("last_page")
-            )
+    try:
+        tag_info = wallhaven_json(f"{wallhaven_tag}{tag_id}").get("data")  # type: ignore
+        tag_name = tag_info.get("name")
+        last_page = wallhaven_json(f"{wallhaven_tag_page}1").get("meta").get("last_page")  # type: ignore
 
-            for page in range(1, last_page + 1):
-                img_dict = wallhaven_json(f"{wallhaven_tag_page}{page}").get("data")
-                page_img_paths = [img.get("path", None) for img in img_dict]
-                img_paths.extend(page_img_paths)
-                print(f"page {page}")
+        # TODO: async
+        for page in range(1, last_page + 1):
+            img_dict = wallhaven_json(f"{wallhaven_tag_page}{page}").get("data")  # type: ignore
+            page_img_paths = [img.get("path", None) for img in img_dict]
+            img_paths.extend(page_img_paths)
+            print(f"page {page}")
 
-            file_path = os.path.join(".", f"{tag_name}_{tag_id}.txt")
-            with open(file_path, "w") as f:
-                f.writelines(f"{img_path}\n" for img_path in img_paths)
+        file_path = os.path.join(".", f"{tag_name}_{tag_id}.txt")
 
-            print(f"{tag_name}_{tag_id}.txt, {len(img_paths)} line(s)")
+        with open(file_path, "w") as f:
+            f.writelines(f"{img_path}\n" for img_path in img_paths)
+
+        print(f"{tag_name}_{tag_id}.txt, {len(img_paths)} line(s)")
+
+    except AttributeError:
+        print("tag_id is wrong")
+        sys.exit()
